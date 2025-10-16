2a088abbea7d4d6b26ad63350f5e8061ccdad936
tanishijanweja <tanishi.janweja@gmail.com>
2025-10-17 00:01:53 +0530

    footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,44 @@
+import { ArrowUpRight } from "lucide-react";
+
+const footerLinks = [
+  {
+    title: "GitHub",
+    href: "",
+  },
+  {
+    title: "LinkedIn",
+    href: "",
+  },
+  {
+    title: "X",
+    href: "",
+  },
+  {
+    title: "YouTube",
+    href: "",
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className="relative">
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
+          <div className="text-white/40">&copy; 2025. All rights reserved.</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8">
+            {footerLinks.map((link) => (
+              <a
+                href="#"
+                key={link.title}
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRight className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
