d4c65b46e2a8633a1ce61d78fbdc851cbb3c0653
tanishijanweja <tanishi.janweja@gmail.com>
2026-03-15 23:07:55 +0530

    error resolve

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -140,7 +140,7 @@ export const ProjectsSection = () => {
                       alt={project.title}
                       className="mt-8 -mb-4 md:mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
                     />
-                  </a>
+                  </a>c
                 </div>
               </div>
             </Card>
